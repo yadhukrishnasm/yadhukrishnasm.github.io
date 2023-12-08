2f483cff19ae04b93d96d7e5884d12779c3f113f
yadhukrishnasm <yadhukrishna.me@gmail.com>
2023-12-08 16:50:20 +0530

    updated name animation.

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const nameDiv = document.querySelector("#myName");
 const image = document.getElementById('myPhoto');
-document.addEventListener('scroll',()=>{
-    if(scrollY*0.0001 > 0.2){
-        nameDiv.setAttribute('sytle',`transform: scale(${scrollY*0.0001});`);
-    }
+console.log(window.innerWidth)
+window.addEventListener('scroll',()=>{
+    console.log(scrollY);
 })
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth:true
+
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]'),
+//     smooth:true
+// })
+
+nameDiv.addEventListener('mousemove',(event)=>{
+
+    let xandY = xY(event.clientX,event.clientY)
+    console.log("x :"+xandY.resX+" y :"+xandY.resY)
+    
+    nameDiv.style["text-shadow"] = `${xandY.resX}px ${xandY.resY}px 1px #F8E9C3`;
 })
+
+
+function xY(x,y){
+    let res={
+        resX:null,
+        resY:null
+    };
+    let widthCenter = (window.innerWidth)/2;
+    let heightCenter = (window.innerHeight)/2;
+    
+        if(x != widthCenter && y!=heightCenter){
+            res ={
+                resX:(x-widthCenter)/10,
+                resY:(y-heightCenter)/10,
+            }
+        }else{
+            return res={
+                resX:0,
+                resY:0
+      
+            }
+        }
+        return res
+    
+}
