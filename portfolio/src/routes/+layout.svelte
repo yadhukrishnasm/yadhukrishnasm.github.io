b4c8d5157b567c86cf7925f84fd20433c76c209a
yadhukrishnasm <yadhukrishna.me@gmail.com>
2025-04-10 01:07:52 +0530

    update : changed to svelte

---
+++ portfolio/src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+
+	const isActive = (route: string) => {
+        if (typeof window !== 'undefined') {
+            const path = window.location.pathname;
+            return path === route;
+        }
+	};
+</script>
+
+<div class=" h-[89vh] md:grid md:grid-cols-6">
+	<nav class="font-dm-serif col-span-1 space-x-10 border-r px-5 pt-10 text-xl">
+		<span><a href="/" class={`hover:underline ${isActive('/') && 'underline'}`}>Intro</a></span>
+	</nav>
+
+	<div class="col-span-5 overflow-y-auto py-10 px-20	">
+		{@render children()}
+	</div>
+</div>
+
+<footer class="h-[11vh] w-full border">
+	<p class="flex h-20 items-center justify-end px-10 text-neutral-700">
+		created with &nbsp; <a href="https://svelte.dev/" class="hover:underline">svelte kit </a> &nbsp;
+		by &nbsp;
+		<a href="https://github.com/yadhukrishnasm" class="font-klemerDisplay text-2xl">SM</a>
+	</p>
+</footer>
---
+++ portfolio/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<div class="flex flex-col md:flex-row-reverse md:justify-between">
+	<div class="transform transition-transform duration-500 ease-in-out">
+		<img
+			src="/images/img.png"
+			alt="Portfolio"
+			class="b h-50 w-50 border border-gray-500 bg-neutral-400 object-cover object-[50%_center] md:h-96 md:w-96 lg:h-[15em] lg:w-[15em] lg:object-[65%_center]"
+		/>
+	</div>
+	<div class="flex w-[60em] flex-col justify-center space-y-5 break-words">
+		<p
+			class="font-klemerDisplay mt-6 pt-5 text-2xl font-bold text-nowrap transition-all md:mt-0 md:text-4xl lg:text-6xl"
+		>
+			YADHU KRISHNA S M
+		</p>
+
+		<p class="">
+			Hi, my name is <span class="font-bold">Yadhu Krishna</span>. Fellow human breed who finds
+			beauty in the uncomplicated!
+		</p>
+	</div>
+</div>
+
+<div class="space-y-2 py-3">
+	<p class="text-lg font-bold">Current Whereabouts</p>
+	<ul class="list-disc pl-5">
+		<li>Developing a POS software called <a href="https://obscidian.site" class="underline">SalesGo</a></li>
+		<li>Enhancing my knowledge in data structures and algorithms</li>
+	</ul>
+</div>
+
